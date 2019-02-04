4252a1c4884c2863dc746526c9a9327e814ef679
Michael <Zuev>
2019-02-04 17:31:04 +0500

    added comments & sout()

--- src/Package1/Main.java
+++ src/Package1/Main.java
@@ -1,3 +1,5 @@
+package Package1;
+
 import java.util.Arrays;
 
 public class Main {
@@ -15,8 +17,8 @@ public class Main {
 
         int tmp; // объявляем переменную
 
-        for (int i = 0; i < arr.length/2; i++) {
-            tmp = arr[i];
+        for (int i = 0; i < arr.length/2; i++) { // идём до середиты, т.к. при каждом проходе, местами меняются 2 эл.
+            tmp = arr[i];                        // получаем за 5 итераций цикла, развернули массив.
             arr[i] = arr[arr.length-i-1];
             arr[arr.length-i-1] = tmp;
         }
@@ -24,3 +26,29 @@ public class Main {
         System.out.println("Перевернутый массив: " + Arrays.toString(arr));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        Исходный массив: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//        Перевернутый массив: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
