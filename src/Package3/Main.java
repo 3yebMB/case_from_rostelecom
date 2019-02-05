03433d71d03449375c36bca713f0246ab8cd71de
3yebmb <3yebmb@gmail.com>
2019-02-05 19:34:08 +0500

    second method has been implemented.

--- src/Package3/Main.java
+++ src/Package3/Main.java
@@ -9,20 +9,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] intArr = {null, 1, 2, null, 7, 1, 2, 1};
-        Double[] doubArr = {null, 1.0, 2.0, null, 7.0, 1.0, 2.0};
+        Object[] intArr = {null, 1, 2, null, 7, 1, 2};
 
-        System.out.println(intArr);
+        System.out.println("Исходный массив : " + Arrays.toString(intArr));
 
         intArr = method2(intArr);
 
-        System.out.println("" + Arrays.toString(intArr));
-//        System.out.println("" + intArr.toString());
+        System.out.println("Массив без повторяющиъся элементов : " + Arrays.toString(intArr));
     }
 
-    private static <T> T[] method1(T[] arr1) {
-        T[] result = null;
-        T[] tmpArr = null;
+    private static Object[] method1(Object[] arr1) {
+        Object[] result = null;
+        Object[] tmpArr = null;
         for (int i = 0; i < arr1.length-1 ; i++) {
             if (arr1[i].equals(arr1[i+1])) {
                 System.arraycopy(arr1, i, tmpArr, 0, tmpArr.length);
@@ -34,23 +32,31 @@ public class Main {
 
     private static <T> T[] method2(T[] arr2) {
         T[] result = null;
-        boolean rem = false;
-
+        boolean isNull = false;
         ArrayList<T> al = new ArrayList<>(Arrays.asList(arr2));
+        int lenArr = 0;
 
         for (int i = 0; i < arr2.length; i++) {
+            lenArr = al.size();
+            if ((arr2[i] == null)) isNull = true;
+            else isNull = false;
+
             for (int j = i+1; j < arr2.length; j++) {
-                if (arr2[i] == null && arr2[j] == null) rem = true;
-                else if (arr2[i].equals(arr2[j])) rem = true;
-                else rem = false;
-                if (rem) {
-                    al.remove(arr2[j]);
+                if (isNull) {
+                    if (arr2[j] == null) {
+                        al.remove(j);
+//                        continue;
+                    }
+                } else {
+                    if (arr2[i].equals(arr2[j]))
+                        al.remove(arr2[j]);
                 }
             }
-            if (rem) { al.remove(arr2[i]); rem = false; }
+            if (lenArr != al.size()) al.remove(arr2[i]);
         }
 
-        System.out.println(al);
+        result = (T[]) al.toArray();
         return result;
+        
     }
 }
